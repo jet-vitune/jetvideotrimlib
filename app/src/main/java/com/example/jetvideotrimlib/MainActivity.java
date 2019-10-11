77d49e506d00ed6668d0a5683f669a753fbd6394
pawanjha123 <pawan972019@gmail.com>
2019-10-11 16:42:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jetvideotrimlib/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jetvideotrimlib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
